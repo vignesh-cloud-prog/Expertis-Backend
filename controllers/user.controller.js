2209b072bb1b42a63185170f43156aa65f678ea9
vignesh-cloud-prog <vigneshun80@gmail.com>
2022-05-16 15:16:16 +0530

    user api
    Signed-off-by: vignesh-cloud-prog <vigneshun80@gmail.com>

---
+++ controllers/user.controller.js
@@ -0,0 +1,105 @@
+const bcrypt = require("bcryptjs");
+const userServices = require("../services/user.services");
+const upload = require("../middlewares/upload.js");
+
+/**
+ * 1. To secure the password, we are using the bcryptjs, It stores the hashed password in the database.
+ * 2. In the SignIn API, we are checking whether the assigned and retrieved passwords are the same or not using the bcrypt.compare() method.
+ * 3. In the SignIn API, we set the JWT token expiration time. Token will be expired within the defined duration.
+ */
+exports.updateProfile = (req, res, next) => {
+  
+  upload(req, res, function (err) {
+    if (err) {
+      next(err);
+    } else {
+      const url = req.protocol + "://" + req.get("host");
+
+      const path =
+        req.file != undefined ? req.file.path.replace(/\\/g, "/") : "";
+
+      var model = {
+        id: req.params.id,
+        name: req.body.name,
+        address: req.body.address,
+        dob: req.body.dob,
+        gender: req.body.gender,
+        pic: path != "" ? url + "/" + path : "",
+      };
+
+      console.log(model,
+        'model');
+
+      userServices.updateProfile(model, (error, results) => {
+        if (error) {
+          return next(error);
+        }
+        return res.status(200).send({
+          message: "Success",
+          data: results,
+        });
+      });
+    }
+  });
+};
+
+exports.register = (req, res, next) => {
+  const { password } = req.body;
+
+  const salt = bcrypt.genSaltSync(10);
+
+  req.body.password = bcrypt.hashSync(password, salt);
+
+  userServices.register(req.body, (error, results) => {
+    if (error) {
+      return next(error);
+    }
+    return res.status(200).send({
+      message: "Success",
+      data: results,
+    });
+  });
+};
+
+exports.login = (req, res, next) => {
+  const { email, password } = req.body;
+
+  userServices.login({ email, password }, (error, results) => {
+    if (error) {
+      return next(error);
+    }
+    return res.status(200).send({
+      message: "Success",
+      data: results,
+    });
+  });
+};
+
+exports.userProfile = (req, res, next) => {
+  return res.status(401).json({ message: "Authorized User!!" });
+};
+
+
+exports.otpLogin = (req, res, next) => {
+  userServices.createNewOTP(req.body, (error, results) => {
+    if (error) {
+      return next(error);
+    }
+    return res.status(200).send({
+      message: "Success",
+      data: results,
+    });
+  });
+};
+
+exports.verifyOTP = (req, res, next) => {
+  userServices.verifyOTP(req.body, (error, results) => {
+    if (error) {
+      return next(error);
+    }
+    return res.status(200).send({
+      message: "Success",
+      data: results,
+    });
+  });
+};
